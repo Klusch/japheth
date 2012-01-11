62be55f5b673abfdcfca61fae7669909ef8a9eca
Alexander Kluge <developer@akluge.de>
2012-01-11 15:07:29 +0100

    Testcase

---
+++ Japeth/test/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package de.linova.test;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
